3ac868716772dd853c1d0a618724d2ebf9bf65a8
StevieGerald <steviegerald007@gmail.com>
2026-07-31 16:30:50 +0100

    Update Meal.js from kmrv-api

--- src/models/Meal.js
+++ src/models/Meal.js
@@ -123,11 +123,6 @@ const mealSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-const mealSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Meal name is required'],
       trim: true,
     },
     description: {
@@ -205,33 +200,6 @@ const mealSchema = new mongoose.Schema(
   {
     timestamps: true,
   }
-      required: [true, 'Meal description is required'],
-    },
-    price: {
-      type: Number,
-      required: [true, 'Meal price is required'],
-      min: [0, 'Price cannot be negative'],
-    },
-    category: {
-      type: String,
-      required: [true, 'Category is required'],
-      enum: ['Main Course', 'Fast Food', 'Drinks', 'Dessert', 'Sides'], // Customize as needed
-    },
-    imageUrl: {
-      type: String,
-      default: '',
-    },
-    isAvailable: {
-      type: Boolean,
-      default: true,
-    },
-    vendor: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', // or 'Vendor' if you have a separate vendor model
-      required: false,
-    },
-  },
-  { timestamps: true }
 );
 
 module.exports = mongoose.model('Meal', mealSchema);
