c1981de7aef7dfbfb5ec3f2b64e41159eebcc487
Gentlemee <ochanyafaith24@gmail.com>
2026-08-02 15:22:53 +0100

    feat: connect customer module routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import mealRoutes from "./routes/meal.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
 import searchRoutes from "./routes/search.routes.js";
 
@@ -12,10 +14,12 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/search", searchRoutes);
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/meals", mealRoutes);
+app.use("/api/cart", cartRoutes);
 app.use("/api/reviews", reviewRoutes);
+app.use("/api/search", searchRoutes);
 
 export default app;
